c71e34bd0ac2db0ef92a75976a9f7c724f1eb6f2
Mankaev_r <romankaev@gmail.com>
2025-06-27 16:10:50 +0300

    Плагин доработан

--- ОВиВК.tab/Виды.panel/Отметки уровней.pushbutton/script.py
+++ ОВиВК.tab/Виды.panel/Отметки уровней.pushbutton/script.py
@@ -57,6 +57,13 @@ from dosymep_libs.bim4everyone import *
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 view = doc.ActiveView
+categoris = [BuiltInCategory.OST_DuctCurves,
+            BuiltInCategory.OST_PipeCurves,
+            BuiltInCategory.OST_MechanicalEquipment,
+            BuiltInCategory.OST_DuctAccessory,
+            BuiltInCategory.OST_PipeAccessory,
+            BuiltInCategory.OST_DuctFitting,
+            BuiltInCategory.OST_PipeFitting]
 
 class LevelDescription:
     def __init__(self, name, elevation):
@@ -65,13 +72,7 @@ class LevelDescription:
 
 class VISElementsFilter(ISelectionFilter):
     def AllowElement(self, element):
-        if element.InAnyCategory([BuiltInCategory.OST_DuctCurves,
-                                  BuiltInCategory.OST_PipeCurves,
-                                  BuiltInCategory.OST_MechanicalEquipment,
-                                  BuiltInCategory.OST_DuctAccessory,
-                                  BuiltInCategory.OST_PipeAccessory,
-                                  BuiltInCategory.OST_DuctFitting,
-                                  BuiltInCategory.OST_PipeFitting]):
+        if element.InAnyCategory(categoris):
             return True
 
         return False
@@ -84,7 +85,7 @@ class TypeAnnotationFilter(ISelectionFilter):
     def AllowElement(self, element):
         fam_name = element.Name
 
-        if element.Category.IsId(BuiltInCategory.OST_GenericAnnotation) and fam_name == "ТипАн_Мрк_B4E_Уровень":
+        if element.Category.IsId(BuiltInCategory.OST_GenericAnnotation) and "ТипАн_Мрк_B4E_Уровень" in fam_name:
             return True
         return False
 
@@ -100,11 +101,15 @@ def get_connectors(element):
 
     if isinstance(element, FamilyInstance) and element.MEPModel.ConnectorManager is not None:
         inst_cons = list(element.MEPModel.ConnectorManager.Connectors)
+        filtered_cons = []
+        for con in inst_cons:
+            if con.Domain in (Domain.DomainHvac, Domain.DomainPiping):
+                filtered_cons.append(con)
 
-        min_z = min(c.Origin.Z for c in inst_cons)
-        max_z = max(c.Origin.Z for c in inst_cons)
+        min_z = min(c.Origin.Z for c in filtered_cons)
+        max_z = max(c.Origin.Z for c in filtered_cons)
 
-        filtered = [c for c in inst_cons if abs(c.Origin.Z - min_z) < 1e-6 or abs(c.Origin.Z - max_z) < 1e-6]
+        filtered = [c for c in filtered_cons if abs(c.Origin.Z - min_z) < 1e-6 or abs(c.Origin.Z - max_z) < 1e-6]
         connectors.extend(filtered)
 
 
@@ -124,6 +129,7 @@ def get_connectors(element):
             connectors.extend(element.ConnectorManager.Connectors)
 
     return connectors
+
 
 def get_connector_coordinates(element):
     """
@@ -147,8 +153,16 @@ def get_connector_coordinates(element):
                 end_point = connector.Origin
                 break
 
-    if start_point is None or end_point is None:
+    if start_point is None and end_point is None:
         forms.alert("Не удалось получить координаты коннекторов.", "Ошибка", exitscript=True)
+
+    if end_point is None:
+        bbox = element.get_BoundingBox(None)
+        min_pt = bbox.Min
+        max_pt = bbox.Max
+        z_diff = max_pt.Z - min_pt.Z
+        end_point = XYZ(start_point.X, start_point.Y, start_point.Z + z_diff)
+
 
     # Получаем координаты начала и конца воздуховода
     start_xyz = XYZ(start_point.X, start_point.Y, start_point.Z)
@@ -157,19 +171,30 @@ def get_connector_coordinates(element):
     return start_xyz, end_xyz
 
 
-def get_selected(filter):
+def get_selected():
     """
     Выделение воздуховодов, труб, арматуры и оборудования.
     """
-    # elements = [uidoc.Document.GetElement(elem_id) for elem_id in uidoc.Selection.GetElementIds()]
-    #
-    # if len(elements) != 0:
-    #     return elements
+
+    elements = [uidoc.Document.GetElement(elem_id) for elem_id in uidoc.Selection.GetElementIds()]
+
+    filter_result = []
+    for element in elements:
+        cat = element.Category
+        if cat is None:
+            continue
+        #print element.Id
+        #print categoris
+        if element.InAnyCategory(categoris):
+            filter_result.append(element)
+
+    if len(filter_result) != 0:
+        return filter_result
 
     try:
         references = uidoc.Selection.PickObjects(
             ObjectType.Element,
-            filter,
+            VISElementsFilter(),
             "Выберите воздуховоды или трубы, нажмите Finish по окончании"
         )
     except Autodesk.Revit.Exceptions.OperationCanceledException:
@@ -181,53 +206,6 @@ def get_selected(filter):
         sys.exit()
 
     return elements
-
-
-def get_reference_from_element(element):
-    """
-    Получаем референс из линейного элемента. Чтоб работало и на воздуховоды и на трубы - берем осевую линию.
-    Если не получается ее найти - возвращаем None, чтоб потом вывести ошибку
-    """
-    options = Options()
-    options.View = view
-    options.ComputeReferences = True
-    options.IncludeNonVisibleObjects = True
-
-    geom_elem = element.get_Geometry(options)
-    line = None
-    lines = []
-    for geo_obj in geom_elem:
-
-        if isinstance(geo_obj, Line):
-            line = geo_obj.Reference
-            lines.append(line)
-
-    return lines[0]
-
-
-def get_mark_endpoint(orientation_name, tag_point):
-    """
-    Получаем координаты марки самой марки и координаты перелома выноски. Вычисляем их на основе точки для которой
-    считаем отметку.
-    """
-    end_epsilon = 3
-    bend_epsilon = 2
-    up_scale = 0.001
-
-    right = view.RightDirection.Normalize()
-    up = view.UpDirection.Normalize().Multiply(up_scale)
-    left = -right
-
-    if orientation_name == LEFT:
-        direction = left + up
-        end_point = tag_point + direction.Multiply(end_epsilon)
-        bend_point = end_point + right.Multiply(bend_epsilon)
-    else:
-        direction = right + up
-        end_point = tag_point + direction.Multiply(end_epsilon)
-        bend_point = end_point - right.Multiply(bend_epsilon)
-
-    return end_point, bend_point
 
 
 def check_base_internal_diff():
@@ -279,30 +257,26 @@ def get_levels_descriptions():
         levels_inst.append(lev_el)
     return levels_inst
 
-def interpolate_point_by_z(p1, p2, z_target):
-    """Находит точку на отрезке p1-p2 с заданным z_target"""
-    if abs(p2.Z - p1.Z) < 1e-6:
-        # Практически горизонтальная труба — Z не изменяется
-        return None
-
-    t = (z_target - p1.Z) / (p2.Z - p1.Z)
-    x = p1.X + t * (p2.X - p1.X)
-    y = p1.Y + t * (p2.Y - p1.Y)
-    return XYZ(x, y, z_target)
-
-
-LEFT = "Слева"
-RIGHT = "Справа"
 
 @notification()
 @log_plugin(EXEC_PARAMS.command_name)
 def script_execute(plugin_logger):
     start_up_checks()
+    elements = get_selected()
 
-    type_annotation = get_selected(TypeAnnotationFilter())[0]
+    reference = uidoc.Selection.PickObject(
+        ObjectType.Element,
+        TypeAnnotationFilter(),
+        "Выберите воздуховоды или трубы, нажмите Finish по окончании"
+    )
+
+    if reference is None or len(elements) == 0:
+        sys.exit()
+
+    type_annotation = doc.GetElement(reference)
     orig_pont = type_annotation.Location.Point
 
-    elements = get_selected(VISElementsFilter())
+
     levels = get_levels_descriptions()
     z_correction = check_base_internal_diff()
 
@@ -318,88 +292,12 @@ def script_execute(plugin_logger):
                 if not (min_z < level.elevation < max_z):
                     continue
 
-
                 new_point = XYZ(connector_coord_1.X, connector_coord_1.Y, level.elevation + z_correction)
-
                 translation = new_point - orig_pont
                 new_tag_Id = ElementTransformUtils.CopyElement(doc, type_annotation.Id, translation)
                 new_tag = doc.GetElement(new_tag_Id[0])
                 new_tag.SetParamValue("Имя уровня", level.name)
-                string_elevation = str(level.elevation * 304.8/1000)
+                string_elevation = "{:+.3f}".format(level.elevation * 304.8 / 1000)
                 new_tag.SetParamValue("Отметка уровня", string_elevation)
-
-    # elements = filter_elements(elements)
-    # #print elements[0]
-    # with (revit.Transaction("BIM: Размещение отметок")):
-    #     type_annotation = type_annotation[0]
-    #     orig_pont = type_annotation.Location.Point
-    #     new_point = XYZ(orig_pont.X, orig_pont.Y, orig_pont.Z + 5)
-    #     translation = new_point - orig_pont
-    #     new_tag = ElementTransformUtils.CopyElement(doc, type_annotation.Id, translation)
-
-
-
-    # with (revit.Transaction("BIM: Копирование")):
-    #     print new_tag
-    #
-    #
-    #     ElementTransformUtils.MoveElement(doc, new_tag[0], translation)
-
-
-    # elements = filter_elements(elements)
-    #
-    # if len(elements) == 0:
-    #     elements = get_selected()
-    #
-    # orientation_name = SelectFromList('Выберите ориентацию марок', [LEFT, RIGHT])
-    # if orientation_name is None:
-    #     sys.exit()
-    #
-    # levels_z = get_levels_z()
-    #
-    # with (revit.Transaction("BIM: Размещение отметок")):
-    #
-    #
-    #     # z-коррекция нужна для случаев когда идет расхождение базовой точки и начала проекта. Оно сбоит при заборе
-    #     # координат коннекторов, из них мы убираем коррекцию.
-    #     # А так же сбой идет при передаче координат в ревит - тут мы наоборот добавляем коррекцию(tag_point)
-    #     # Срабатывает и когда базовая точка ниже начала и выше
-    #     z_correction = check_base_internal_diff()
-    #
-    #     for element in elements:
-    #         coord_list = get_connector_coordinates(element)
-    #         connector_coord_1 = coord_list[0]
-    #         connector_coord_2 = coord_list[1]
-    #
-    #         min_z = min(connector_coord_1.Z, connector_coord_2.Z) - z_correction
-    #         max_z = max(connector_coord_1.Z, connector_coord_2.Z) - z_correction
-    #
-    #         ref = get_reference_from_element(element)
-    #         if ref is None:
-    #             break
-    #
-    #         for z in levels_z:
-    #             if not (min_z < z < max_z):
-    #                 continue
-    #
-    #             tag_point = interpolate_point_by_z(connector_coord_1, connector_coord_2, z + z_correction)
-    #             end_point, bend_point = get_mark_endpoint(orientation_name, tag_point)
-    #
-    #             doc.Create.NewSpotElevation(
-    #                 view,
-    #                 ref,
-    #                 tag_point, # The point which the spot elevation evaluate.
-    #                 bend_point, # The bend point for the spot elevation.
-    #                 end_point, # The end point for the spot elevation.
-    #                 tag_point, # The actual point on the reference which the spot elevation evaluate.
-    #                 True
-    #             )
-    #
-    # if ref is None:
-    #     forms.alert(
-    #         "На виде не найдены осевые линии. Отметки не были размещены.",
-    #         "Ошибка",
-    #         exitscript=True)
-
 
 script_execute()
